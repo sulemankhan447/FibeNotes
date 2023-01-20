58636400896b6e48cf50f31548fc2269d4992507
Suleman Khan <sulemankhan447@gmail.com>
2023-01-20 19:50:53 +0530

    1) Done setup for navigation components i.e nav graph 2) created add and edit fragments

---
+++ app/src/main/java/com/fibenotes/fragments/AddNoteFragment.kt
@@ -0,0 +1,20 @@
+package com.fibenotes.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.fibenotes.R
+
+
+class AddNoteFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_note, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/fibenotes/fragments/EditNoteFragment.kt
@@ -0,0 +1,21 @@
+package com.fibenotes.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fibenotes.R
+
+
+class EditNoteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_note, container, false)
+    }
+
+}
